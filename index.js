59edf31fdda054f7f3e693dea78d7ca8d8922abb
fdolin <fdolin@users.noreply.github.com>
2017-10-05 17:24:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var soap2json = require("express-soap2json")
 
 var app = require("express")()
 
-var SOAP_SERVER = "http://www.webservicex.net"
+var SOAP_SERVER = "http://www.ekan-tremblaye1.fr/logistique/wsdl/GetStockArtByEcom.wsdl"
 var JSON_PREFIX = "api"
 
 app.use(soap2json(SOAP_SERVER, JSON_PREFIX))
